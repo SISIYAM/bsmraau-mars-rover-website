f495de3429db28c18cec6cc35266089b0bcd600e
MD Saymum Islam Siyam <si31siyam@gmail.com>
2024-10-23 22:30:08 +0600

    increase cookie time to 1 month

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -91,8 +91,15 @@ const loginEjsUser = async (req, res) => {
     const token = jwt.sign(payload, JWT_SECRET, { expiresIn: TOKEN_EXPIRY });
 
     // set JWT token in cookie
-    res.cookie("token", token, { httpOnly: true });
-    res.cookie("isLoggedIn", true, { httpOnly: true, secure: true });
+    res.cookie("token", token, {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+    res.cookie("isLoggedIn", true, {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: true,
+    });
     res.redirect("/dashboard");
   } catch (error) {
     console.error("Error logging in user:", error);
